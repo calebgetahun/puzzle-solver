ca8183c93c8fdd6e940194c1b275019ad56a36aa
Caleb Getahun <calebgetahun011@gmail.com>
2025-07-23 22:14:11 -0400

    removed logging until essential usage required

--- puzzle_solver_api/routers/uploads_router.py
+++ puzzle_solver_api/routers/uploads_router.py
@@ -1,40 +1,23 @@
-import logging
 from typing import List
 from fastapi import APIRouter, UploadFile, File, HTTPException
 from puzzle_solver_core.src.image_processing_core import image_bytes_to_colors, process_image_files
 
 router = APIRouter()
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s | %(levelname)s | %(name)s | %(message)s',
-)
-
-logger = logging.getLogger(__name__)
-
 @router.post("/images")
 async def upload_images(images: List[UploadFile] = File(...)):
-    logger.info(f"Received {len(images)} images")
-    for image in images:
-        logger.info(f"Image: {image.filename} - content_type: {image.content_type}")
     try:
         cube_image_bytes = [await image.read() for image in images]
-        logger.info(f"Image byte sizes: {[len(b) for b in cube_image_bytes]}")
         cube_faces = image_bytes_to_colors(cube_image_bytes)
         return {"cube_faces": cube_faces}
     except Exception as e:
-        logger.exception("Error while processing images")
-        raise HTTPException(status_code=500, detail=str(e))
-    
+        raise HTTPException(status_code=500, detail="Server error")
+
 @router.post("/hsv_values")
 async def hsv_cube_faces(images: List[UploadFile] = File(...)):
-    logger.info(f"Received {len(images)} images")
-    for image in images:
-        logger.info(f"Image: {image.filename} - content_type: {image.content_type}")
     try:
         cube_image_bytes = [await image.read() for image in images]
         hsv_values = process_image_files(cube_image_bytes)
         return {"hsv_cube": hsv_values}
     except Exception as e:
-        logger.exception("Error while processing for hsv values")
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail="Server error")
