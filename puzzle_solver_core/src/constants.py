bab38ef6feeb90886ffca76055ddc54e5f761e53
Caleb Getahun <calebgetahun011@gmail.com>
2025-07-23 21:15:16 -0400

    trimmed dockerfile and added dockerignore. Made some structural changes to clean up old code

--- puzzle_solver_core/src/constants.py
+++ puzzle_solver_core/src/constants.py
@@ -1,11 +1,4 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-CUBE_FOLDER = BASE_DIR / "assets" / "cube_images"
-FRAME_RATIO_CONSTANT = 0.3
 CUBE_FACE_POSITIONING = (1, 3, 5)
-CUBE_FACE_NOTATION = ["UP", "RIGHT", "FRONT", "DOWN", "LEFT", "BACK"]
-CUBE_FACE_COLORS = []
 
 COLOR_MAP = {
     "R": [(0, 100, 75), (9, 255, 255)],
@@ -15,15 +8,6 @@ COLOR_MAP = {
     "B": [(100, 100, 75), (140, 255, 255)],
     "R2": [(160, 100, 75), (179 , 255, 255)],
     "W": [(0, 0, 150), (180, 99, 255)]
-}
-
-COLOR_NUMBER_MAP = {
-    "white": 1,
-    "green": 2,
-    "blue": 3,
-    "yellow": 4,
-    "orange": 5,
-    "red": 6
 }
 
 COLOR_LETTER_MAP = {
